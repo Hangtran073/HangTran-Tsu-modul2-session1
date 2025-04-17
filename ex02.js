954793d8be6861e4295c2815c8c584e2f0ddd781
Hangtran073 <hangt6465@gmail.com>
2025-04-17 20:04:52 +0900

    upload session01 ex02

---
+++ ex02.js
@@ -0,0 +1,13 @@
+const checkParity = (a) => {
+    if (isNaN(a)) {
+        console.log(`${a} not is number`)
+    }
+    else if (a % 2 === 0) {
+        console.log(`${a} is an even number`)
+    } else {
+        console.log(`${a} is odd`)
+    }
+}
+checkParity("a");
+checkParity(10);
+checkParity(7);
